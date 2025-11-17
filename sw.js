5b8157e75e87274dce52f743f1298b04d78602aa
y8yh4gxphw-ops <y8yh4gxphw@privaterelay.appleid.com>
2025-11-17 03:12:55 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,2 +0,0 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('kfz-v1').then(c=>c.addAll(['./','./index.html','./style.css','./script.js','./questions.js'])))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
